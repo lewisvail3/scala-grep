7c7cbf2ef9d46bd78f940610af2240a2982996ca
Lewis Vail <lewisvail3@gmail.com>
2016-01-17 19:02:15 -0500

    Initial checkin of scala grep with minimal argument support.

---
+++ main/scala/com/lewisvail3/grep/Grep.scala
@@ -0,0 +1,75 @@
+package com.lewisvail3.grep
+
+import io.Source
+import util.matching.Regex
+import java.io.FileNotFoundException
+
+object Grep {
+
+  private def printUsage() {
+    // Full usage print out
+//    println("usage: grep [-abcDEFGHhIiJLlmnOoqRSsUVvwxZ] [-A num] [-B num] [-C[num]]")
+//    println("\t[-e pattern] [-f file] [--binary-files=value] [--color=when]")
+//    println("\t[--context[=num]] [--directories=action] [--label] [--line-buffered]")
+//    println("\t[--null] [pattern] [file ...]")
+    
+    // implemented usage print out
+    println("usage: grep [pattern] [file ...]")
+  }
+
+  def main(args: Array[String]) {
+
+    if (args.length == 0) {
+      printUsage()
+    } else {
+
+      // parse args
+      var pattern = "";
+      var fileNames: List[String] = Nil
+      def nextOption(map: Map[Symbol, String], list: List[String]): Map[Symbol, String] = {
+        list match {
+          case Nil => map
+          // TODO: uncomment these as they become implemented
+//          case "-A" :: value :: tail =>
+//            nextOption(map ++ Map('linesAfterMatch -> value), tail)
+//          case "-B" :: value :: tail =>
+//            nextOption(map ++ Map('linesBeforeMatch -> value), tail)
+          case option :: tail if option.startsWith("-") =>
+            Console.err.println("option " + option + " is not supported")
+            throw new IllegalArgumentException
+          case patternArg :: tail if pattern == "" =>
+            pattern = patternArg
+            nextOption(map, tail)
+          case filename :: tail =>
+            fileNames = fileNames :+ filename
+            nextOption(map, tail)
+        }
+      }
+
+      try {
+        var options = nextOption(Map(), args.toList)
+
+        fileNames.foreach { filename => grepFile(filename, pattern, fileNames.size > 1) }
+      } catch {
+        case ex: IllegalArgumentException => System.exit(1)
+      }
+    }
+  }
+
+  private def grepFile(filename:String, pattern:String, prependFilename:Boolean) {
+    try {
+    	val source = Source.fromFile(filename)
+    			try {
+    				source.getLines().filter { line => pattern.r().findFirstIn(line).nonEmpty }
+    				.map { line => if (prependFilename) filename + ":" + line else line }
+    				.foreach { println }
+    			} finally {
+    				source.close()
+    			}
+    } catch {
+      case ex: FileNotFoundException =>
+        Console.err.println(filename + ": No such file or directory")
+    }
+  }
+
+}
